adf272fd25da905e8c528054e5ff960b5e2d897d
svecchaApps <sveccha.apps@gmail.com>
2025-12-23 20:17:42 +0530

    Update cartRoutes.js

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -14,6 +14,8 @@ router.post("/CreateCart", authMiddleware, cartController.upsertCart);
 
 router.get("/cart-id/:userId", authMiddleware, cartController.getCartIdByUserId);
 router.get("/cart-details/:userId", authMiddleware, cartController.getCartDetailsByUserId);
+router.get("/total/:userId", cartController.getCartTotalByUserId);
+router.get("/total", cartController.getCartTotalByUserId); 
 router.put("/update-address/:userId", authMiddleware, cartController.updateCartAddress);
 
 module.exports = router;
